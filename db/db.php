3234e7406d735992e3717780b7d2f70628c4b9c0
henil0604 <62794871+henil0604@users.noreply.github.com>
2020-07-11 15:41:41 +0530

    Update db.php

--- db/db.php
+++ db/db.php
@@ -1,16 +1,15 @@
 <?php
 
 
-$server = "localhost";
-$user = "root";
-$dbpassword = "henil0604";
+$server = "_Your Server_";
+$user = "_Username_";
+$dbpassword = "_Your Database Password_";
 $db = "emaillogin";
 
 $con = mysqli_connect($server, $user, $dbpassword, $db);
 
 
 if ($con) {
-    $_SESSION['msg'] = "Fill All the Information";
 } else {
     $_SESSION['msg'] = "No Internet Connection";
 }
